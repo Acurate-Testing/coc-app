4ab1e58a437627efbdb64cb4b38e1dc37b2429a2
Colin Shah <colin.s@brilworks.com>
2025-06-02 12:07:53 +0530

    fix forgot password issue

--- src/middleware.ts
+++ src/middleware.ts
@@ -19,6 +19,7 @@ export async function middleware(request: NextRequest) {
     "/login",
     "/register",
     "/reset-password",
+    "/set-password",
     "/_next",
     "/static",
     "/api/auth",
